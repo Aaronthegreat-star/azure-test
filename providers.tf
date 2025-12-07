b18ed94989e2bd04eac62926e5e78d469b9e363d
Onyeani Njoku victor <126138357+Aaronthegreat-star@users.noreply.github.com>
2025-12-07 19:30:01 +0100

    Update azurerm provider version and required version

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.8.0"
     }
   }
-  required_version = ">= 1.0.0"
+  required_version = ">=1.9.0"
 }
 
 
